0de7a685a1793674dc7d04de5d1a6e6076d8cd3e
Shogo Yoshizawa <shogo.yoshizawa.2000@gmail.com>
2026-06-11 22:55:27 +0900

    crosstermを用いた、最小のmain loopを実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, Event, KeyCode, KeyModifiers},
+    terminal,
+};
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    terminal::enable_raw_mode()?;
+
+    let result = run();
+
+    terminal::disable_raw_mode()?;
+    result
+}
+
+fn run() -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    loop {
+        let Event::Key(key) = event::read()? else {
+            continue;
+        };
+
+        match (key.code, key.modifiers) {
+            (KeyCode::Char('d'), KeyModifiers::CONTROL) => break,
+            (KeyCode::Enter, _) => {
+                // raw mode では \r\n を明示する必要がある
+                print!("\r\n");
+                stdout.flush()?;
+            }
+            (KeyCode::Char(c), _) => {
+                print!("{c}");
+                stdout.flush()?;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
